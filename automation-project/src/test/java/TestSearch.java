e5540826350a312cb7eead2d45e04f72d1895cb9
SAKSHI RAJ <SAKSHI RAJ@192.168.0.4>
2024-04-04 10:08:11 +0530

    Code

--- automation-project/src/test/java/TestSearch.java
+++ automation-project/src/test/java/TestSearch.java
@@ -2,7 +2,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+import org.openqa.selenium.JavascriptExecutor;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -16,21 +18,44 @@ WebDriver driver;
 	setup();
 	
 	driver=new ChromeDriver();
+	driver.manage().window().maximize();
 	driver.get("https://www.amazon.in/");
+	
 	
 	}
 @When("User searches for Laptop")
 public void SearchLaptop() {
 	driver.findElement(By.xpath("//input[contains(@id,'twotabsearchtextbox')]")).sendKeys("Laptop");
 	driver.findElement(By.xpath("//input[contains(@id,'nav-search-submit-button')]")).click();
-	driver.close();
+	 JavascriptExecutor js = (JavascriptExecutor) driver;
+	 js.executeScript("window.scrollBy(0, 600)");
 }
+	 
+	 @When("Search for the element" )
+			public void ClickOnElemt() {
+		 //JavascriptExecutor js = (JavascriptExecutor) driver;
+		 
+		// js.executeScript("arguments[0].click();", "//h2[@class=\"a-size-mini a-spacing-none a-color-base s-line-clamp-2\"])[2]");
+			//driver.findElement(By.xpath("(//a[contains(@class,'l s-underline-text s-underline-link-text s-link-style a-text-normal')])[2]")).click();		
+			driver.findElement(By.xpath("(//div[contains(@class,'a-section a-spacing-none puis-padding-right-small s-title-instructions-style')])[2]")).click();
+			 
+			
+}
+	
+
+/*
 @Then("Select laptop")
 public void MoveToCart() {
-	driver.findElement(By.xpath("//*[@id=\"search\"]/div[1]/div[1]/div/span[1]/div[1]/div[3]/div/div/div/div/span/div/div/div/div[2]/div/div/div[1]/h2/a/span")).click();
-	//driver.getWindowHandle()
-	//driver.switchTo().window
-
+	
+	for (String windowHandle : driver.getWindowHandles()) {
+           if (!windowHandle.equals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+               break;
+            }
+        }
+driver.findElement(By.xpath("(//*[@title='Add to Shopping Cart'])[2]")).click();
+driver.close();*/
 
 }
-}
+
+
